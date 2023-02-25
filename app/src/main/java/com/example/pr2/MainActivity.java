1ec8eb1e2cfddc5aa6fc5fe99114fe33046f0914
Dmitrii <dmitrii_anastasiadi@yandex.ru>
2023-02-25 20:54:14 +0300

    exchanging info btw activities

--- app/src/main/java/com/example/pr2/MainActivity.java
+++ app/src/main/java/com/example/pr2/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.pr2;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Instrumentation;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,24 +28,38 @@ public class MainActivity extends AppCompatActivity {
     Button enter_system_btn;
     ImageView startup_logo;
     EditText login_edit_text;
+    boolean dark_theme;
 
-
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dark_theme = false;
+        intent = new Intent(this, Profile.class);
         startup_logo = (ImageView) findViewById(R.id.image_view);
         enter_system_btn = (Button) findViewById(R.id.button);
         startup_page_text_view = (TextView) findViewById(R.id.text_view);
         login_edit_text = (EditText) findViewById(R.id.login_edit_text);
         enter_system_btn.setOnClickListener(view -> {
-//            startup_page_text_view.setText(R.string.system_in_page_text);
-//            startup_logo.setImageResource(R.drawable.s21_vector_logo);
-            Intent intent = new Intent(this, Profile.class);
-
             String login = login_edit_text.getText().toString().split("@")[0];
             intent.putExtra("login", login);
-            startActivity(intent);
+            intent.putExtra("dark_theme_on", dark_theme);
+            mStartForResult.launch(intent);
         });
     }
+    ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                Intent res_intent = result.getData();
+                Bundle args = res_intent.getExtras();
+                if ((boolean) args.get("dark_theme_on")) {
+                    startup_logo.setImageResource(R.drawable.s21_vector_logo);
+                    dark_theme = true;
+                } else {
+                    startup_logo.setImageResource(R.drawable.s21_logo);
+                    dark_theme = false;
+                }
+            }
+    );
 }
--- app/src/main/java/com/example/pr2/Profile.java
+++ app/src/main/java/com/example/pr2/Profile.java
@@ -2,6 +2,7 @@ package com.example.pr2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CompoundButton;
@@ -20,16 +21,17 @@ public class Profile extends AppCompatActivity {
 
         hello_text_view = (TextView) findViewById(R.id.hello_text_view);
         dark_theme_switch = (Switch) findViewById(R.id.dark_theme_switch);
-        Bundle arguments = getIntent().getExtras();
-        String login = arguments.get("login").toString();
+        Bundle args = getIntent().getExtras();
+        String login = args.get("login").toString();
         hello_text_view.setText("Привет, " + login + "!");
+        dark_theme_switch.setChecked((boolean) args.get("dark_theme_on"));
 
         dark_theme_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                if (isChecked) {
-                    Toast.makeText(getApplicationContext(), "YAS", Toast.LENGTH_SHORT).show();
-                }
+                getIntent().putExtra("dark_theme_on", isChecked);
+                setResult(0, getIntent());
+                finish();
             }
         });
     }
